3f876eda60d1d41daae1219077c2cf9ecb71f7c4
moises <moisessantosddt@gmail.com>
2023-01-24 20:08:54 -0300

    Jogo de xadrez

---
+++ jogoDeXadrez/Program.cs
@@ -0,0 +1,56 @@
+﻿
+using tabuleiro;
+using Pecas;
+
+namespace jogoDeXadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                PartidaDeXadrez partidas = new PartidaDeXadrez();
+
+                while (!partidas.terminada)
+                {
+                    try
+                    {
+                        Console.Clear();
+                        Tela.imprimirPartida(partidas);
+
+                        Console.WriteLine();
+                        Console.Write("\nOrigem: ");
+                        Posicao origem = Tela.LerPosicaoXadrez().ToPosicao();
+                        partidas.ValidarPosicaoDeOrigem(origem);
+
+                        bool[,] posicoesPossiveis = partidas.Tab.Peca(origem).MovimentosPossiveis();
+
+                        Console.Clear();
+                        Tela.imprimirTabuleiro(partidas.Tab, posicoesPossiveis);
+
+                        Console.WriteLine();
+                        Console.Write("Destino: ");
+                        Posicao destino = Tela.LerPosicaoXadrez().ToPosicao();
+                        partidas.ValidarPosicaoDeDestino(origem, destino);
+
+                        partidas.RealizarJogada(origem, destino);
+                    }
+                    catch (TabuleiroException e)
+                    {
+                        Console.WriteLine(e.Message);
+                        Console.ReadLine();
+                    }
+
+                }
+                Console.Clear();
+                Tela.imprimirPartida(partidas);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+
+}
---
+++ jogoDeXadrez/tabuleiro/Peca.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tabuleiro
+{
+    abstract class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int QtdMovimentos { get; set; }
+        public Tabuleiro Tab { get; set; }
+
+        public Peca() { }
+
+        public Peca(Tabuleiro tab, Cor cor)
+        {
+            Posicao = null;
+            Tab = tab;
+            Cor = cor;
+            QtdMovimentos = 0;
+        }
+        // é um metodo para adicionar uma quantidade de movimentos a peça
+        public void IncrementarMov()
+        {
+            QtdMovimentos++;
+        }
+        // é um metodo para subtrair uma quantidade de movimentos a peça
+        public void DecrementarMov()
+        {
+            QtdMovimentos--;
+        }
+        // ele verifica se e possivel a pedra se mover conforme as regras
+        public bool existeMovimentos()
+        {
+            bool[,] mat = MovimentosPossiveis();
+            for (int i = 0; i < Tab.Linhas; i++)
+            {
+                for (int j = 0; j < Tab.Linhas; j++)
+                {
+                    if (mat[i, j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool MovimentoPossivel(Posicao pos)
+        {
+            return MovimentosPossiveis()[pos.Linha, pos.Coluna];
+        }
+
+        public abstract bool[,] MovimentosPossiveis();
+    }
+}
